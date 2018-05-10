4a1bdcf66f92be9e4fb61c523aae8ee949331ac1
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-05-10 21:25:41 +0200

    returning json instead of Response object

--- unbabel/app/api_client.py
+++ unbabel/app/api_client.py
@@ -3,7 +3,7 @@ import json
 import requests
 import concurrent.futures
 import time
-from app.database_wrapper import DatabaseWrapper
+from database_wrapper import DatabaseWrapper
 
 UNBABEL_API_BASE_URL = os.environ.get('UNBABEL_API_BASE_URL', 'https://sandbox.unbabel.com/tapi/v2')
 TRANSLATION_URL = '{}/translation/'.format(UNBABEL_API_BASE_URL)
@@ -29,18 +29,20 @@ class APIClient(object):
                 new_object = {'text': text, 'source_language': source_language, 'target_language': target_language}
                 data['objects'].append(new_object)
 
-        return requests.patch(TRANSLATION_URL, json.dumps(data), headers=HEADERS)
+        response = requests.patch(TRANSLATION_URL, json.dumps(data), headers=HEADERS)
+        return json.loads(response.content)
 
     def get_translation(self, uid):
-        return requests.get('{}{}'.format(TRANSLATION_URL, uid), headers=HEADERS)
+        response = requests.get('{}{}'.format(TRANSLATION_URL, uid), headers=HEADERS)
+        return json.loads(response.content)
 
     def get_translations(self, translation_uids):
         executor = concurrent.futures.ThreadPoolExecutor()
         future_results = [executor.submit(self.get_translation, uid) for uid in translation_uids]
         concurrent.futures.wait(future_results)
-        return [json.loads(future.result().content) for future in future_results]
+        return [future.result() for future in future_results]
 
-    def update_tranlations_until_completed(self, translation_uids, max_wait_in_seconds=300, wait_time_between_updates=20):
+    def update_translations_until_completed(self, translation_uids, max_wait_in_seconds=300, wait_time_between_updates=20):
         incomplete_translations_uids = translation_uids
         max_wait_time = time.time() + max_wait_in_seconds
         while (time.time() <= max_wait_time):
--- unbabel/tests/test_api_client.py
+++ unbabel/tests/test_api_client.py
@@ -1,8 +1,8 @@
 import pytest
 import json
 from mock import Mock
-from app.api_client import APIClient
-from app.database_wrapper import DatabaseWrapper
+from api_client import APIClient
+from database_wrapper import DatabaseWrapper
 
 @pytest.fixture(scope='session')
 def db_wrapper():
@@ -17,11 +17,9 @@ def run_around_tests(db_wrapper):
 def test_translate():
     """Checking that we can request translation of multiple texts at once"""
     texts = ['hello', 'world']
-    response = APIClient().translate(texts, source_language='en', target_languages=['pt', 'fr'])
-    json_response = json.loads(response.content)
+    json_response = APIClient().translate(texts, source_language='en', target_languages=['pt', 'fr'])
     objects = json_response['objects']
 
-    assert response.status_code == 202
     assert len(objects) == 4
     assert set([obj['source_language'] for obj in objects]) == {'en'}
     assert set(([obj['text'] for obj in objects])) == set(['world', 'hello'])
@@ -29,26 +27,23 @@ def test_translate():
 
 def test_get_translation():
     """Retrieving translation by uid"""
-    response = APIClient().translate(['hello'], source_language='en', target_languages=['fr'])
-    json_response = json.loads(response.content)
+    json_response = APIClient().translate(['hello'], source_language='en', target_languages=['fr'])
     translation_request = json_response['objects'][0]
-    response = APIClient().get_translation(translation_request['uid'])
-    json_response = json.loads(response.content)
-
-    assert response.status_code == 200
+    json_response = APIClient().get_translation(translation_request['uid'])
+    print(json_response)
     assert json_response['uid'] == translation_request['uid']
     assert json_response['text'] == translation_request['text']
     assert json_response['source_language'] == translation_request['source_language']
     assert json_response['target_language'] == translation_request['target_language']
 
-def test_update_tranlations_until_completed(db_wrapper):
+def test_update_translations_until_completed(db_wrapper):
     """fetching and persisting translations until their status becomes 'completed' """
     api_client = APIClient(db_wrapper)
     mock_get_translations = []
     mock_get_translations.append([{'uid': 'fake_uid', 'status': 'new'}])
     mock_get_translations.append([{'uid': 'fake_uid', 'status': 'completed'}])
     api_client.get_translations = Mock(side_effect=mock_get_translations)
-    api_client.update_tranlations_until_completed(translation_uids=['fake_uid'], wait_time_between_updates=0)
+    api_client.update_translations_until_completed(translation_uids=['fake_uid'], wait_time_between_updates=0)
     translation = db_wrapper.db.translations.find_one()
 
     assert db_wrapper.db.translations.count() == 1
